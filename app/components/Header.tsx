8f4505c621ec832b09ab6ff5a3777f94ac07e5aa
Connie <conniedpaula@gmail.com>
2023-11-30 10:09:43 +0000

    feat: add link to home to logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 
 import logoImage from '@/public/logo.svg';
+import Link from 'next/link';
 
 function Header() {
   return (
     <header className='mx-auto my-0 flex w-full max-w-[1180px] items-center justify-between gap-6 rounded-lg bg-gradient-to-br from-neutral-100 to-neutral-200/30 p-3'>
-      <Image src={logoImage} alt='Small MIDI controller' width={150} />
+      <Link href='/' passHref>
+        <Image src={logoImage} alt='Small MIDI controller' width={150} />
+      </Link>
       <h1 className='text-lg  font-bold text-red-900'>Premium MIDI Controllers</h1>
     </header>
   );
