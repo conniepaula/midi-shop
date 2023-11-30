60a2df6f471d6cad9c2f1ec56718f880e2205019
Connie <conniedpaula@gmail.com>
2023-11-30 10:18:03 +0000

    style: add external container to page for a better ux on mobile, update button styles, add option to use icon in button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,19 @@
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  icon?: React.ReactNode;
+  iconPosition?: 'start' | 'end';
 }
+
 function Button(props: ButtonProps) {
-  const { children, ...rest } = props;
+  const { children, icon, iconPosition = 'start', ...rest } = props;
   return (
-    <button className='text-md mt-auto cursor-pointer rounded-md border-0 bg-red-900 px-4 py-3 font-bold text-neutral-100 hover:bg-red-950/80 hover:transition-all'>
+    <button
+      className={`text-md mt-auto cursor-pointer rounded-md border-0 bg-red-900 px-4 py-3 font-bold text-neutral-100 hover:bg-red-800 hover:transition-all ${
+        !!icon && 'flex items-center justify-center gap-2'
+      } ${iconPosition === 'start' ? 'flex-row' : 'flex-row-reverse'}`}
+      {...rest}
+    >
+      {icon}
       {children}
     </button>
   );
